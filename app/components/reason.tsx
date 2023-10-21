8c6bb35c0cae3fab8a38cccfab5a890af29a4fea
Vithushan <63534975+ACuteWoof@users.noreply.github.com>
2023-10-21 08:30:51 +0000

    nm

--- app/components/reason.tsx
+++ app/components/reason.tsx
@@ -1,5 +1,5 @@
 import { Montserrat } from "next/font/google"
-import { motion, useInView } from "framer-motion"
+import { motion, useInView, } from "framer-motion"
 import { useRef } from "react"
 
 const montserrat = Montserrat({ subsets: ['latin'], weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"] })
@@ -9,34 +9,38 @@ export default function Reason({ index, title, reason }: { index: number, title:
     const ref = useRef(null)
     const isInView = useInView(ref);
 
-
     const ref1 = useRef(null)
     const isInView1 = useInView(ref1);
 
 
-    return <div className="w-full xl:max-w-[50vw] py-12 px-12 xl:h-screen xl:p-36 flex flex-col justify-center gap-4 xl:gap-12">
-        <motion.h1
-            initial={{ opacity: 0, y: 100 }}
-            animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }}
-            transition={{
-                duration: 0.5,
-                ease: "easeInOut",
-            }}
-            ref={ref}
-            className={"lg:text-6xl 2xl:text-[5vw] font-semibold " + montserrat.className} >
-            {title}
-        </motion.h1>
-        <motion.p
-            initial={{ opacity: 0, y: 100 }}
-            animate={isInView1 ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }}
-            transition={{
-                duration: 0.5,
-                ease: "easeInOut",
-                delay: 0.25
-            }}
-            ref={ref1}
-            className="text-lg 2xl:text-[1vw] font-light">
-            {reason}
-        </motion.p>
+    return <div
+
+        className="w-full xl:max-w-[50vw] py-12 px-12 xl:h-screen xl:p-36 flex flex-col justify-center xl:gap-12">
+        <div
+            className="flex flex-col gap-4"
+        >
+            <motion.h1
+                initial={{ opacity: 0, y: 100 }}
+                animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }}
+                transition={{
+                    duration: 0.5,
+                    ease: "easeInOut",
+                }}
+                ref={ref}
+                className={"lg:text-6xl 2xl:text-[5vw] font-semibold " + montserrat.className} >
+                {title}
+            </motion.h1>
+            <motion.p
+                initial={{ opacity: 0, y: 100 }}
+                animate={isInView1 ? { opacity: 1, y: 0 } : { opacity: 0, y: 100 }}
+                transition={{
+                    duration: 0.5,
+                    ease: "easeInOut",
+                    delay: 0.25
+                }}
+                ref={ref1}
+                className="text-lg 2xl:text-[1vw] font-light">
+                {reason}
+            </motion.p></div>
     </div>
 }
