31ffeafd0d36b111d61676a64e947463cf9b9ce5
ACuteWoof <sutharshanvithushan@gmail.com>
2024-06-09 23:43:24 +0530

    Revert "projects"
    This reverts commit 624915269a58956879399ff21236325e3361acae.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,9 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "0": "url('https://unsplash.com/photos/7XRcZ9fbkRQ/download?force=true')",
-        "2": "url('https://unsplash.com/photos/eFbxYl9M_lc/download?force=true')",
-        "1": "url('https://unsplash.com/photos/4ZFeOnVwoek/download?force=true')",
-	"3": "url('https://unsplash.com/photos/6lQDFGOB1iw/download?force=true')"
+        "0": "url(https://images.unsplash.com/photo-1620812097331-ff636155488f?auto=format&w=1280)",
+        "1": "url(/images/bg-1.jpeg)",
+        "2": "url('https://images.unsplash.com/photo-1566305977571-5666677c6e98?auto=format&w=1280&fit=crop&q=80&w=2145&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
       },
     },
   },
